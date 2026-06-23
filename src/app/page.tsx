0314fe1f09b5b049119b97172bebb42832d01748
dpernek <pernekblackberry@gmail.com>
2026-06-23 12:50:38 +0200

    Fix import PopularProducts default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Hero } from "@/components/home/Hero";
 import { FeaturedCategories } from "@/components/home/FeaturedCategories";
-import { PopularProducts } from "@/components/home/PopularProducts";
+import PopularProducts from "@/components/home/PopularProducts";
 import { Benefits } from "@/components/home/Benefits";
 import { CTASection } from "@/components/home/CTASection";
 import type { Metadata } from "next";
